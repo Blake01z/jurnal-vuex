a4a4dada8bc2bd5bab527d6115aca4590f583f4f
Daniel Correa <daniel.correa92@hotmail.com>
2022-05-22 23:11:47 -0500

    Agregamos rutas para la navegacion

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,8 +4,18 @@
 
     <h1>Hola Mundo</h1>
 
-    <button class="btn btn-primary">Primary</button>
-    <button class="btn btn-success">Primary</button>
-    <button class="btn btn-secondary">Primary</button>
+    <button @click="goToDayBook" class="btn btn-primary">Primary</button>
+    <button @click="goToDayBook" class="btn btn-success">Primary</button>
+    <button @click="goToDayBook" class="btn btn-secondary">Primary</button>
   </div>
 </template>
+
+<script>
+  export default{
+    methods:{
+      goToDayBook(){
+        this.$router.push({name: 'daybook'})
+      }
+    }
+  }
+</script>
